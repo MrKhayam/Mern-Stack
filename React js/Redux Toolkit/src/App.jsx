07bbd3477c0ec7c78d511cec2ca8cc8cc3da7466
unknown <ijazkhayam@gmail.com>
2024-10-13 04:45:51 +0500

    counter project with redux

--- React js/Redux Toolkit/src/App.jsx
+++ React js/Redux Toolkit/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Counter from './components/Counter'
 
 const App = () => {
   return (
     <>
-      <div className="w-full h-screen bg-zinc-900">
-        
+      <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+        <Counter />
       </div>
     </>
   )
