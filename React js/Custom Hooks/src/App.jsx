e35e3f5ebbbff570bce3fce099796023d95b8ae7
unknown <ijazkhayam@gmail.com>
2024-10-12 05:16:41 +0500

    custom hooks

--- React js/Custom Hooks/src/App.jsx
+++ React js/Custom Hooks/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Counter from './components/Counter';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='text-white flex items-center justify-center w-full h-screen bg-zinc-900'>
+        <Counter />
+        </div>
+    </>
   )
 }
 export default App;
---
+++ React js/Custom Hooks/src/custom hooks/useCount.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const useCount = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => { setCount((prevCount) => prevCount + 1) }
+    const decrement = () => { setCount((prevCount) => prevCount - 1) }
+
+    return {count, increment, decrement}
+}
+
+export default useCount;
