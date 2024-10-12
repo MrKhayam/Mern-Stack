897734049cd02e9c047f43cc48e24db8f0f8d282
unknown <ijazkhayam@gmail.com>
2024-10-13 04:49:07 +0500

    todo project setup

--- React js/Redux Toolkit/src/App.jsx
+++ React js/Redux Toolkit/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Counter from './components/Counter'
 
 const App = () => {
   return (
     <>
-      <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
-        <Counter />
+      <div className="w-full h-screen bg-zinc-900">
+        
       </div>
     </>
   )
--- React js/Redux Toolkit/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  counterReducer  from "../features/countSlice"
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
