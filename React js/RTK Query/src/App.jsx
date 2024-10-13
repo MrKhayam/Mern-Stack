07d11ac19558cb513695b075100805d5b7737422
unknown <ijazkhayam@gmail.com>
2024-10-13 14:09:16 +0500

    getting started with rtk query

--- React js/RTK Query/src/App.jsx
+++ React js/RTK Query/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="w-full h-screen bg-zinc-900 border border-zinc-900">
+        
+      </div> 
+    </>
   )
 }
 
