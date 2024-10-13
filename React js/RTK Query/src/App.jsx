e3678ef3de343b4276fb9cf09c250ba5a1582b8b
unknown <ijazkhayam@gmail.com>
2024-10-13 15:26:25 +0500

    github users with rtk query

--- React js/RTK Query/src/App.jsx
+++ React js/RTK Query/src/App.jsx
@@ -1,9 +1,23 @@
+import React from "react";
+import { useGetUsersQuery } from "./slices/apiSlice";
+import Card from "./components/Card";
+
 const App = () => {
+  const { data, isLoading, error } = useGetUsersQuery();
+
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+  if (error) {
+    return <div>Error : {error.message}</div>;
+  }
 
   return (
     <>
       <div className="w-full min-h-screen bg-zinc-900 p-32 flex flex-wrap gap-4">
-        
+        {data && data.map((user) => {
+          return <Card key={user.id} user={user} />
+        })}
       </div>
     </>
   );
--- React js/RTK Query/src/components/Card.jsx
+++ React js/RTK Query/src/components/Card.jsx
@@ -1,4 +1,4 @@
-const Card = () => {
+const Card = ({user}) => {
   return (
     <div className="w-52 h-52 bg-white rounded-lg flex flex-col p-4">
       
