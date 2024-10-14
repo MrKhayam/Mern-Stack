422b3dec0976dc8455c1f4452b90d92dfd21eaaa
unknown <ijazkhayam@gmail.com>
2024-10-14 05:16:24 +0500

    update

--- React js/RTK Query/src/components/Card.jsx
+++ React js/RTK Query/src/components/Card.jsx
@@ -1,7 +1,8 @@
 const Card = ({user}) => {
   return (
-    <div className="w-52 h-52 bg-white rounded-lg flex flex-col p-4">
-      
+    <div className="w-52 h-52 bg-white border border-blue-300 rounded-lg flex flex-col">
+      <img className="h-[80%] object-cover rounded-t-lg" src={user.avatar_url} alt="" />
+      <h1 className="h-[20%] text-center capitalize font-semibold flex items-center justify-center">{user.login}</h1>
     </div>
   );
 };
