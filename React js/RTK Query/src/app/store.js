6f6a3d41dd5a648680ef2309ba5eaa10f473c7b5
unknown <ijazkhayam@gmail.com>
2024-10-14 05:24:46 +0500

    update

--- React js/RTK Query/src/app/store.js
+++ React js/RTK Query/src/app/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
 
+
 export const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
+        [apiSlice.reducerPath] : apiSlice.reducer,
     },
-    middleware: (getDefaultMiddleware) => 
+    middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
-
-});
-
+})
